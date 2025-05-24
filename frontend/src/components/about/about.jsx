eec22fbcab6e64bb3397df81a935cbac5aaf0ba7
Oscar Martin <oscar.martin.2112@lacetania.cat>
2025-05-24 17:24:41 +0200

    a

--- frontend/src/components/about/about.jsx
+++ frontend/src/components/about/about.jsx
@@ -31,7 +31,7 @@ const values = [
 
 const About = () => (
   <div className="flex items-center justify-center bg-gradient-to-br from-green-100 to-blue-50">
-    <div className="bg-white shadow-xl p-4 rounded-2xl max-w-3xl w-full animate-fadeIn">
+    <div className="bg-white shadow-xl p-8 rounded-2xl max-w-3xl w-full animate-fadeIn">
 
       <h1 className="text-4xl font-extrabold mb-2 text-center text-green-700 tracking-tight">
         Acerca de PetConnect
